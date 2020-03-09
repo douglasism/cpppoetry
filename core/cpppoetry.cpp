8bb418dae3eb6272e5b397a4a3b7f674ae356e7a
Douglas MacIver <douglasmaciver@hotmail.com>
2020-03-08 17:42:01 -0800

    hollow world

---
+++ core/cpppoetry.cpp
@@ -0,0 +1,12 @@
+﻿// cpppoetry.cpp : Defines the entry point for the application.
+//
+
+#include "cpppoetry.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "hollow world" << endl;
+	return 0;
+}
---
+++ core/cpppoetry.h
@@ -0,0 +1,8 @@
+﻿// cpppoetry.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
